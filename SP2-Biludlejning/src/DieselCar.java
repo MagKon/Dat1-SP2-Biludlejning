98f1c35a7c678635d2cf73773bd07fea0d7865d0
TheLonelyDude <magnus.kongstad@gmail.com>
2022-10-25 12:24:01 +0200

    xd

--- SP2-Biludlejning/src/DieselCar.java
+++ SP2-Biludlejning/src/DieselCar.java
@@ -1,2 +1,42 @@
-public class DieselCar {
+public class DieselCar extends AFuelCar {
+    private boolean particleFilter;
+
+    public DieselCar(boolean particleFilter, int kmPrLitre) {
+        super(kmPrLitre);
+        this.particleFilter = particleFilter;
+    }
+
+    public boolean hasParticleFilter() {
+        return particleFilter;
+    }
+
+    public int getRegistrationFee() {
+        int fee = 0;
+//        if (getKmPrLitre() < 20) {
+//            fee += 1000;
+//        } else if (getKmPrLitre() < 50) {
+//            fee += 2000;
+//        } else {
+//            fee += 3000;
+//        }
+//        if (hasParticleFilter()) {
+//            fee += 500;
+//        }
+        return fee;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %d", getFuelType(), hasParticleFilter() ? "with" : "without", getKmPrLitre());
+    }
+
+    @Override
+    public String getFuelType() {
+        return null;
+    }
+
+    @Override
+    public int getKmPrLitre() {
+        return 0;
+    }
 }
--- SP2-Biludlejning/src/ElectricCar.java
+++ SP2-Biludlejning/src/ElectricCar.java
@@ -1,4 +1,36 @@
-public class ElectricCar {
-    public ElectricCar() {
+public class ElectricCar extends ACar {
+    private final int batteryCapacity;
+    private final int maxRange;
+
+    public ElectricCar(String registrationNumber, String make, String model, int numberOfDoors, int batteryCapacity, int maxRange) {
+        super(registrationNumber, make, model, numberOfDoors);
+        this.batteryCapacity = batteryCapacity;
+        this.maxRange = maxRange;
+
+    }
+
+    @Override
+    public String getRegistrationNumber() {
+        return null;
+    }
+
+    @Override
+    public String getMake() {
+        return null;
+    }
+
+    @Override
+    public String getModel() {
+        return null;
+    }
+
+    @Override
+    public int getNumberOfDoors() {
+        return 0;
+    }
+
+    @Override
+    public int getRegistrationFee() {
+        return 0;
     }
 }
