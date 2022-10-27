ff5cc448a81a818801d9e31faf41676f0ea3db21
Magkon <63054857+TheLonelyDude@users.noreply.github.com>
2022-10-27 18:32:14 +0200

    changed to private ArrayList

--- SP2-Biludlejning/src/FleetOfCars.java
+++ SP2-Biludlejning/src/FleetOfCars.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class FleetOfCars {
-    ArrayList<Car> fleet = new ArrayList<Car>();
+    private ArrayList<Car> fleet = new ArrayList<Car>();
 
     public void addCar(Car car) {
         fleet.add(car);
